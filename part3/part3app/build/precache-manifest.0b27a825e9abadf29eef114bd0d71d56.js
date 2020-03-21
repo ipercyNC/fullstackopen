aac3c586c47c7a42820055a75df02af2f929fd75
Ian Percy <ipercy@pdx.edu>
2020-03-20 19:47:33 -0700

    adding part3app

---
+++ part3/part3app/build/precache-manifest.0b27a825e9abadf29eef114bd0d71d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1361e228a0f42b232aa99523f5fe72b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f770ecf07192b189e5",
+    "url": "/static/css/main.05f8c557.chunk.css"
+  },
+  {
+    "revision": "92509a2300bdbe3170ec",
+    "url": "/static/js/2.ca2e7571.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ca2e7571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f770ecf07192b189e5",
+    "url": "/static/js/main.7198183f.chunk.js"
+  },
+  {
+    "revision": "e3280c1cf29291f4ca23",
+    "url": "/static/js/runtime-main.455a30d0.js"
+  }
+]);
